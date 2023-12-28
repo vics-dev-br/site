a0c04b638ea313b666c3bea332262f6a13fe8fdc
cristianojuris <55989450+cristianojuris@users.noreply.github.com>
2023-12-27 23:56:56 -0300

    images

--- src/app/modules/blog/blog.type.ts
+++ src/app/modules/blog/blog.type.ts
@@ -7,6 +7,7 @@ export interface Post
         blockquote: string;
         paragraphs: string[];
         conclusion: string;
+        image: string;
     },
     relationships: {
         author: {
