7a971ff5cb520a57551b902db927dc36a4004360
cristianojuris <55989450+cristianojuris@users.noreply.github.com>
2023-12-31 17:19:12 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import {TabsModule} from 'ngx-tabset';
 import { AppComponent } from './app.component';
@@ -7,6 +7,7 @@ import { PreloaderComponent } from './modules/home/components/preloader/preloade
 import { ServicesTwoComponent } from './modules/home/components/services-two/services-two.component';
 import { appRoutes } from './app-routing.module';
 import { ExtraOptions, PreloadAllModules, RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 const routerConfig: ExtraOptions = {
@@ -24,6 +25,12 @@ const routerConfig: ExtraOptions = {
         BrowserModule,
         BrowserAnimationsModule,
         RouterModule.forRoot(appRoutes, routerConfig),
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        }),
     ],
     providers: [],
     bootstrap: [AppComponent]
